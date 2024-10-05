8ac42f4677ee8a683a6670589a40e383062c31ce
ThisIsMeAdityaSingh <thisismeadityasingh99@gmail.com>
2024-10-05 15:10:16 +0530

    Initialized supabase

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const server = express();
-const PORT = 5000;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, function(){
     console.log(`Express listening to PORT - ${PORT}`);
